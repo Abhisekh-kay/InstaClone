d6eb6ecc2442ebf9fdb7163c787ee3dadd44dadd
Abhisekh Kayastha <abhisekhkayasth@gmail.com>
2023-12-27 21:42:47 +0545

    MONGODB Setup

--- server/index.js
+++ server/index.js
@@ -6,12 +6,13 @@ const multer = require('multer')
 const helmet = require('helmet')
 const morgan = require('morgan')
 require('dotenv').config()
-import path from "path";
-import { fileURLToPath } from "url";
+const path = require("path") ;
+const { fileURLToPath } = require("url");
 
 // Middleware Configuration
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename)
+// const __filename = fileURLToPath(import.meta.url);
+const currentFilePath = __filename;
+const currentDirectory = path.dirname(currentFilePath)
 const app = express();
 app.use(express.json());
 app.use(helmet())
@@ -20,7 +21,7 @@ app.use(morgan("common"));
 app.use(bodyparser.json({limit: "30mb", extended: true}))
 app.use(bodyparser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors())
-app.use("/assets",express.static(path.join(__dirname, 'public/assetss')))
+app.use("/assets",express.static(path.join(currentDirectory, 'public/assetss')))
 
 // File Storage
 const storage = multer.diskStorage({
@@ -33,3 +34,20 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({storage});
+
+// MONGODB Setup
+// const mongooseOptions = {
+//     useCreateIndex: true, // Use the new option to opt-in for index creation
+//     useFindAndModify: false, // Use the new option to opt-out from using deprecated methods
+//     useNewUrlParser: true, // This is no longer needed, but you can keep it until the next major version
+//     useUnifiedTopology: true // This is no longer needed, but you can keep it until the next major version
+//   };
+
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(()=>{
+    app.listen(process.env.PORT, ()=> console.log(`Server Port: ${process.env.PORT}`));
+})
+.catch((error) => console.log(`${error} did not connect`));
+
+// app.listen(process.env.PORT, () => console.log("Server is running on port ", + process.env.PORT))
